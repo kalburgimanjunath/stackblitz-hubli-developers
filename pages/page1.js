93f4a37b0a01232515c8996f06beaf90a3023fd9
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-07-28 13:48:42 +0530

    2 files modified

--- pages/page1.js
+++ pages/page1.js
@@ -1,7 +1,7 @@
 const Section1 = ({ blubs }) => {
   return (
-    <div className="brand2 p-5 m-5">
-      <div className="flex grid grid-cols-3 justify-around w-full mt-5">
+    <div className="bg-orange-200 p-5 min-h-full">
+      <div className="flex h-50 grid grid-cols-3 justify-around w-full mt-5">
         {blubs &&
           blubs.map((item) => {
             return (
@@ -20,32 +20,32 @@ const Section1 = ({ blubs }) => {
 };
 const Header = () => {
   return (
-    <div className="flex">
+    <div className="flex sticky top-0 brand1 shadow-md">
       <div className="p-2 m-1 hover:text-pink-500">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
-          class="w-6 h-6"
+          className="w-6 h-6"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
           />
         </svg>
       </div>
       <div className="flex justify-end w-full text-green-800 font-bold">
         <div className="p-2 m-1 hover:bg-gray-300 cursor-pointer rounded-md">
-          Home
+          <a href="#section1">Home</a>
         </div>
         <div className="p-2 m-1 hover:bg-gray-300 cursor-pointer rounded-md">
-          Project
+          <a href="#section4"> Project</a>
         </div>
         <div className="p-2 m-1 hover:bg-gray-300 cursor-pointer rounded-md">
-          Contact us
+          <a href="#section6">Contact us</a>
         </div>
       </div>
     </div>
@@ -83,7 +83,7 @@ const Section3 = ({ blubs }) => {
         {blubs &&
           blubs.map((item) => {
             return (
-              <div className="rounded border-2 border-black p-3 m-2 brand2">
+              <div className="rounded border-2 border-black p-3 m-2 bg-orange-300">
                 <div className="text-md font-bold">{item.title}</div>
                 <div className="text-md italic">{item.author}</div>
               </div>
@@ -120,7 +120,7 @@ const Footer = () => {
         <span className="p-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
+            className="h-5 w-5"
             fill="currentColor"
             viewBox="0 0 24 24"
           >
@@ -130,7 +130,7 @@ const Footer = () => {
         <span className="p-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
+            className="h-5 w-5"
             fill="currentColor"
             viewBox="0 0 24 24"
           >
@@ -140,7 +140,7 @@ const Footer = () => {
         <span className="p-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
+            className="h-5 w-5"
             fill="currentColor"
             viewBox="0 0 24 24"
           >
@@ -150,7 +150,7 @@ const Footer = () => {
         <span className="p-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
+            className="h-5 w-5"
             fill="currentColor"
             viewBox="0 0 24 24"
           >
@@ -174,13 +174,13 @@ export default function Page1() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
-          class="w-20 h-20"
+          className="w-20 h-20"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"
           />
         </svg>
@@ -194,13 +194,13 @@ export default function Page1() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
-          class="w-20 h-20"
+          className="w-20 h-20"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
           />
         </svg>
@@ -214,13 +214,13 @@ export default function Page1() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
-          class="w-20 h-20"
+          className="w-20 h-20"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
           />
         </svg>
@@ -258,27 +258,49 @@ export default function Page1() {
       image: '',
     },
   ];
+  const RecentProjects = ({ items }) => {
+    return (
+      <div className="recent-projects flex grid grid-cols-3 p-3">
+        {items &&
+          items.map((item) => {
+            return (
+              <div className="p-2 w-full hover:scale-125 transition duration-500 cursor-pointer">
+                <img src={item.image} className="rounded-md w-full h-48 " />
+              </div>
+            );
+          })}
+      </div>
+    );
+  };
+  const projects = [
+    { image: 'https://picsum.photos/seed/picsum/200/300' },
+    { image: 'https://picsum.photos/seed/picsum/200/300' },
+    { image: 'https://picsum.photos/seed/picsum/200/300' },
+    { image: 'https://picsum.photos/seed/picsum/200/300' },
+    { image: 'https://picsum.photos/seed/picsum/200/300' },
+  ];
 
   return (
-    <div className="brand1 container h-50">
+    <div className="brand1 container h-50 bg-fixed">
       <Header />
-      <div className="p-5 h-50 min-h-full m-5">
+      <div className="p-5 h-50 max-h-full m-5" id="section0">
         <h1 className="text-8xl font-bold mt-3 mb-3 text-green-800">
           Revolutionizing Hubli Landscapes. Discover Dream Properties.
         </h1>
 
         <button className="primary">Start Exploring</button>
       </div>
-      <Section1 blubs={blubs} />
-      <Section2 blubs={blubs1} />
-      <Section3 blubs={blubs2} />
-      <div className="p-5 text-center">
+      <Section1 blubs={blubs} id="section1" />
+      <Section2 blubs={blubs1} id="section2" />
+      <Section3 blubs={blubs2} id="section3" />
+      <div className="p-5 text-center" id="section4">
         <h1 className="m-2 text-3xl">Let’s create visionary spaces.</h1>
         <div>
           <button className="secondary">Get Started</button>
         </div>
       </div>
-      <div className="p-5 text-center">
+      <RecentProjects items={projects} id="section5" />
+      <div className="p-5 text-center" id="section6">
         <h1 className="m-2 text-3xl">Get Started</h1>
         <div className="m-2">
           Ready to embark on a land development journey with the best of Hubli?
